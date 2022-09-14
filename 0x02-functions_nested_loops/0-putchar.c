e5a64223f40f6ddaf82665b5e099e1a698a94748
mwangiii <mwangi8kevin@gmail.com>
2022-09-14 07:46:55 -0700

    low level programming

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,31 @@
-#include <stdio.h>
-#include <main.h>
+#include <unistd.h>
+#include "main.h"
 
-int main (void)
-{  
-           char word [8] ="_putchar";
-           int i; 
-           
-           for (i = 0; i < 8; i++)
-           {
-               _putchar (word[i]); 
-           }
-	  _putchar('\n');
+/*
+ * main - check description
 
-       	  return (0);
+ *
 
+ * Description: It prints the word Holberton, followed by a new line.
+
+ *
+
+ * Return: Always 0.
+
+ */
+int main(void)
+
+{
+	char word[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+
+		_putchar(word[i]);
+
+	}
+
+	_putchar('\n');
+	return (0);
 }
