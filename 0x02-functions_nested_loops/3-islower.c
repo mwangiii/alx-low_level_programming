3b6196357f4633662707239e3c355f356955e461
mwangiii <mwangi8kevin@gmail.com>
2022-09-14 09:04:24 -0700

    nested files

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,28 @@
 #include "main.h"
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * _islower - check main
+ * @i: An integer input
+ * Description: Write a function that checks for lowercase character
+ * Return: 0 or 1.
  */
-int main(void)
-{
-    int r;
 
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+int _islower(int i)
+{
+
+	if (i >= 'a' && i <= 'z')
+
+	{
+
+		return (1);
+
+	}
+
+	else
+
+	{
+
+		return (0);
+
+	}
 }
