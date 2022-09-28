ddb6d890425ef53e5c2e65dd771c53901f0fc1db
mwangiii <mwangi8kevin@gmail.com>
2022-09-28 04:15:51 -0700

    recursions

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,7 @@
+void _puts_recursion(char *s);
+1-print_rev_recursion.c
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
