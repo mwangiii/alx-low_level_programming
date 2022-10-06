c6de14954c71093bf3e7d68d332415a6bf964a47
mwangiii <mwangi8kevin@gmail.com>
2022-10-06 05:59:50 -0700

    more malloc,free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+*Function that allocates memory using malloc.
+* Return: a pointer with the memory location.
+*/
+
+void *malloc_checked(unsigned int b)
+{
+
+	void *ptr = NULL;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+
+	return (ptr);
+}
